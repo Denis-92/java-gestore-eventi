0a41166aba3e8bd3652a137c80e7600711b6f546
Denis-92 <d.mozenic@gmail.com>
2023-02-03 17:00:32 +0100

    add class ProgrammEventi

---
+++ src/org/lessons/java/events/ProgrammEventi.java
@@ -0,0 +1,31 @@
+package org.lessons.java.events;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammEventi {
+	
+	private String titolo;
+	private List<Evento> eventi;
+	
+	public ProgrammEventi (Evento evento) {
+		this.titolo = evento.getTitolo();
+		eventi = new ArrayList<>();
+	}
+	
+	public void addEventoToList (Evento evento) {
+		eventi.add(evento);
+	}
+	
+	// TODO - IMPLEMENTARE IL CONTROLLO SE DATA == TRUE
+	public List<Evento> getEventiPerDate(LocalDate data) {
+		List<Evento> eventiodierni = new ArrayList<>();
+		for (int i = 0; i < eventi.size(); i++) {
+			if (true)
+				eventiodierni.add(eventi.get(i));
+			}
+		return eventiodierni;
+	}
+
+}
