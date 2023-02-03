cbb3f2685fe6d768bf5b5864553004042f4ac504
Denis-92 <d.mozenic@gmail.com>
2023-02-03 11:18:15 +0100

    add exceptions for: data, postiTotali

--- src/org/lessons/java/events/Evento.java
+++ src/org/lessons/java/events/Evento.java
@@ -8,11 +8,25 @@ public class Evento {
 	private LocalDate data;
 	private int postiTotali;
 	private int postiPrenotati;
+	private LocalDate dataAttuale = LocalDate.now();
 	
-	public Evento (String titolo, LocalDate data, int postiTotali) {
+	public Evento (String titolo, LocalDate data, int postiTotali) throws Exception {
 		this.titolo = titolo;
 		this.data = data;
+		validazioneData(data);
 		this.postiTotali = postiTotali;
+		validazionePostiTotali(postiTotali);
 		this.postiPrenotati = 0;
 	}
+	
+	public void validazioneData(LocalDate data) throws Exception {
+		if (dataAttuale.isBefore(data))
+			throw new Exception();
+	}
+	
+	public void validazionePostiTotali(int postiTotali) throws Exception {
+		if (postiTotali < 1)
+			throw new Exception();
+	}
+	
 }
