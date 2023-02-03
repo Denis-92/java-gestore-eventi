9ded1f9a00fcdac1c89a8e8a75588eaaf73d1241
Denis-92 <d.mozenic@gmail.com>
2023-02-03 11:10:46 +0100

    change type data

--- src/org/lessons/java/events/Evento.java
+++ src/org/lessons/java/events/Evento.java
@@ -1,13 +1,15 @@
 package org.lessons.java.events;
 
+import java.time.LocalDate;
+
 public class Evento {
 	
 	private String titolo;
-	private String data;
+	private LocalDate data;
 	private int postiTotali;
 	private int postiPrenotati;
 	
-	public Evento (String titolo, String data, int postiTotali) {
+	public Evento (String titolo, LocalDate data, int postiTotali) {
 		this.titolo = titolo;
 		this.data = data;
 		this.postiTotali = postiTotali;
